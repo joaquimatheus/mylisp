2cb575f03724fa17ea6e2c4c3999263ffd6596d4
Matheus Zakken <matheus@marcogabriel.com.br>
2022-12-16 10:33:01 -0300

    modify some parsing rules and evaluates operators

--- parsing.c
+++ parsing.c
@@ -23,6 +23,75 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+enum { LVAL_NUM, LVAL_ERR };
+
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return x;
+}
+
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+void lvav_print(lval v) {
+    switch (v.type) {
+        case LVAL_NUM: printf("$li", v.num); break;
+
+        case LVAL_ERR:
+           if (v.err == LERR_DIV_ZERO) {
+                printf("Error: Division By Zero!");
+           }
+           if (v.err == LERR_BAD_OP) {
+               printf("Error: Invalid Operator");;
+           }
+           if (v.err == LERR_BAD_NUM) {
+               printf("Error: Invalid Number");
+           }
+        break;
+    }
+}
+
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main (int argc, char** argv) {
     mpc_parser_t* Number   = mpc_new("number");
     mpc_parser_t* Operator = mpc_new("operator");
@@ -43,13 +112,15 @@ int main (int argc, char** argv) {
 
     while (1) {
         char* input = readline("lispy> ");
-
         add_history(input);
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            mpc_ast_print(r.output);
+
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
+
         } else {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
